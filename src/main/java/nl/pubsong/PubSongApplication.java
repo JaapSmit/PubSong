3c9e04cd7bdc6ee33e17268fc24d03b7faab938b
Jaap Smit <jaapsmit94@hotmail.com>
2016-11-15 13:27:33 +0100

    Start project

---
+++ src/main/java/nl/pubsong/PubSongApplication.java
@@ -0,0 +1,12 @@
+package nl.pubsong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubSongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubSongApplication.class, args);
+	}
+}
